8d4eda366317313b8904372a9a9fb8f81535d93e
Gabrielle Palado <paladogabr@ecs.vuw.ac.nz>
2018-05-08 16:08:34 +1200

    Added class and constructor description to ResponseHandler

--- src/main/java/com/android/volley/toolbox/ResponseHandler.java
+++ src/main/java/com/android/volley/toolbox/ResponseHandler.java
@@ -5,6 +5,11 @@ import android.support.annotation.GuardedBy;
 import com.android.volley.NetworkResponse;
 import com.android.volley.Response;
 
+/**
+ * Base class for handling response parsing and delivery.
+ *
+ * @param <T> Parsed type for this response
+ */
 public abstract class ResponseHandler<T> {
     /** Lock to guard mListener as it is cleared on cancel() and read on delivery. */
     private final Object mLock = new Object();
@@ -12,6 +17,11 @@ public abstract class ResponseHandler<T> {
     @GuardedBy("mLock")
     private Response.Listener<T> mListener;
 
+    /**
+     * Creates a new ResponseHandler.
+     *
+     * @param listener Listener to receive the response
+     */
     public ResponseHandler(Response.Listener<T> listener){
         mListener = listener;
     }
